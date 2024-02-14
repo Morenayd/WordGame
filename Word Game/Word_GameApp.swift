eeeaa75e03b2fde50b0e019fbe21b631b702ea56
Jesutofunmi <tofdebby@gmail.com>
2024-02-14 15:06:13 +0100

    Initial Commit

---
+++ Word Game/Word_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_GameApp.swift
+//  Word Game
+//
+//  Created by Jesutofunmi Adewole on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Word_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
